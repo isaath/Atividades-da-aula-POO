8de40d7bb419e3211370786fb59ca8343c0105d9
isaath <2024301756@aluno.canoas.ifrs.edu.br>
2026-08-03 16:27:51 -0300

    fiz o contrutor

--- atv_avaliativa/Estudante.java
+++ atv_avaliativa/Estudante.java
@@ -1,7 +1,17 @@
 package atv_avaliativa;
 
+import java.util.ArrayList;
+
 public class Estudante {
     private String nome;
     private ArrayList<Double> notas;
+
+
+    //contrutor
+    public Estudante(String nome, ArrayList<Double> notas){
+        this.nome = nome;
+        this.notas = notas;
+    }
+
 
 }
