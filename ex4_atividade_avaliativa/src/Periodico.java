efe1f0e4be1f6249a1a7fb2fe632c06d84a559c5
isaath <2024301756@aluno.canoas.ifrs.edu.br>
2026-08-22 21:04:18 -0300

    testando

--- ex4_atividade_avaliativa/src/Periodico.java
+++ ex4_atividade_avaliativa/src/Periodico.java
@@ -1,17 +1,18 @@
-public class Periodico extends ItensBlibioteca{
+public class Periodico extends ItensBlibioteca {
     private int numeroVolume;
 
     //contrutor
-    public Periodico(String titulo, int numeroVolume){
+    public Periodico(String titulo, int numeroVolume) {
         super(titulo);
         this.numeroVolume = numeroVolume;
     }
 
     //gets e sets
-    public int getNumeroVolume(){
+    public int getNumeroVolume() {
         return numeroVolume;
     }
-    public void setNumeroVolume(int numeroVolume){
+
+    public void setNumeroVolume(int numeroVolume) {
         this.numeroVolume = numeroVolume;
     }
 }
